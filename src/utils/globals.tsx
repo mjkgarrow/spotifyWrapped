3d1daae3b8c509531a4ce00ec49f73650e41c155
socialtonics <mjkgarrow@gmail.com>
2023-09-10 22:31:53 +1000

    ✨ feat: added colour animation to background

---
+++ src/utils/globals.tsx
@@ -0,0 +1,20 @@
+export const colours: string[] = [
+
+    '#ff99c8',
+    '#fcf6bd',
+    '#d0f4de',
+    '#a9def9',
+    '#e4c1f9',
+
+    '#ff595e',
+    '#ffca3a',
+    '#8ac926',
+    '#3CADF3',
+    '#9F6FE2',
+
+    '#f4a261',
+    '#2a9d8f',
+    '#e9c46a',
+    '#4D8EA8',
+    '#e76f51',
+]
