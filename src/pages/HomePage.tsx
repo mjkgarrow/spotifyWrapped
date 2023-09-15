0fc17b715a50b244293e7b6a66c2caf3922c6e7b
socialtonics <mjkgarrow@gmail.com>
2023-09-15 15:29:28 +1000

    🔥: removed unused props and api calls

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,16 @@
-import { useEffect } from "react"
 import { useGlobalState } from "../context/globalState"
 import CircleLoading from "../components/CircleLoading"
 import TopTracksSection from "../components/TopTracksSection"
 import WelcomeSection from "../components/WelcomeSection"
 import EmptySection from "../components/EmptySection"
 import TopArtistsSection from "../components/TopArtistsSection"
-import TopGenresSection from "../components/TopGenres"
+import ListeningSection from "../components/ListeningSection"
 
-type props = {
-    timeRange: string
-}
 
-export default function HomePage(props: props) {
-    const { timeRange } = props
-    const { data, checkEmpty, makeApiCall } = useGlobalState()
-
-    useEffect(() => {
-        makeApiCall('artists', timeRange)
-        makeApiCall('tracks', timeRange)
-
-    }, [timeRange])
+export default function HomePage() {
+    const { data, checkEmpty } = useGlobalState()
 
     // ---- TODO -----
-
-    // LISTENING PERSONALITY SECTION (get genres from artists, compare with popularity, compare with volume of top tracks)
     // RECOMMENDATIONS SECTION
 
     return (
@@ -39,7 +26,7 @@ export default function HomePage(props: props) {
 
                     {data.tracks.length && <TopTracksSection tracks={data.tracks.slice(0, 5)} />}
 
-                    {data.tracks.length && <TopGenresSection tracks={data.tracks.slice(0, 99)} />}
+                    {data.tracks.length && <ListeningSection tracks={data.tracks.slice(0, 99)} />}
 
                     <EmptySection backgroundColour="bg-orange-400" />
                 </main>
