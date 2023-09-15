ae3adf2614cb151e5da1a8e54b7397f273ad62f6
socialtonics <mjkgarrow@gmail.com>
2023-09-15 15:29:00 +1000

    🔥: removed unused props

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,8 @@ import UserInfo from './UserInfo';
 import { useGlobalState } from '../context/globalState';
 import TimeSelector from './TimeSelector';
 
-type props = {
-    set: any,
-    get: string,
-}
-
-export default function Header(props: props) {
+export default function Header() {
     const { isAuth } = useGlobalState()
-    const { set, get } = props
 
     return (
         <header className='w-screen flex h-16 justify-between items-center px-8 bg-[#1ed760] fixed z-50 top-0'>
@@ -19,7 +13,7 @@ export default function Header(props: props) {
                 {isAuth() ? <UserInfo /> : <img src='/assets/images/Spotify-Icon-png-rgb-black.png' className='w-10' alt='spotify logo'></img>}
             </div>
             <div className='w-1/3 flex justify-center'>
-                {isAuth() ? <TimeSelector set={set} get={get} /> : <div className='text-3xl font-bold'>Wrap Me!</div>}
+                {isAuth() ? <TimeSelector /> : <div className='text-3xl font-bold'>Wrap Me!</div>}
             </div>
             <div className='w-1/3 flex justify-end'>
                 {isAuth() ? <LogoutBtn /> : <LoginBtn />}
