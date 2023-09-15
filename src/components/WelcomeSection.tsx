0bede841ceaf4bdb1c6dce88a12853dc507a89f6
socialtonics <mjkgarrow@gmail.com>
2023-09-15 15:30:22 +1000

    🐛: fixed bouncing icon jittering

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -1,13 +1,14 @@
 import { gsap } from "gsap"
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useLayoutEffect } from "react"
+import { useEffect, useRef } from "react"
 
 
 export default function WelcomeSection() {
+    const comp = useRef(null)
 
-    useLayoutEffect(() => {
-        ScrollTrigger.refresh()
+    useEffect(() => {
         const ctx = gsap.context(() => {
+            gsap.to('#arrow', { duration: 0.6, y: 20, ease: "circ.in", repeat: -1, yoyo: true });
+
             gsap.to('#welcome-pattern', {
                 scrollTrigger: {
                     trigger: '#welcome-pattern',
@@ -16,21 +17,20 @@ export default function WelcomeSection() {
                     pinSpacing: false,
                 }
             })
-        })
+        }, comp)
 
         return () => ctx.revert()
     }, []);
 
 
     return (
-        <section>
+        <section ref={comp}>
             <div id='welcome-pattern' className="w-screen h-screen flex flex-col justify-center items-center -z-10">
                 <div className="bg-white p-4 rounded-3xl drop-shadow-xl">
                     <h1 className="text-2xl sm:text-4xl font-bold text-center">Scroll to view your wrap</h1>
                     <div className="w-full flex justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="60px" width="60px" viewBox="0 0 384 512" className="bg-white rounded-full p-2 top-16 absolute animate-bounce fill-purple-500"><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" id='arrow' height="60px" width="60px" viewBox="0 0 384 512" className="bg-white rounded-full p-2 top-12 absolute fill-purple-500"><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" /></svg>
                     </div>
-
                 </div>
             </div>
         </section>
