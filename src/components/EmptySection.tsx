ec513c4f8e29fd4e3cd156b5adc1e9f0c5669fad
socialtonics <mjkgarrow@gmail.com>
2023-09-15 15:43:02 +1000

    💄: added a goodbye message

--- src/components/EmptySection.tsx
+++ src/components/EmptySection.tsx
@@ -7,6 +7,12 @@ export default function EmptySection(props: props) {
     const { backgroundColour } = props
 
     return (
-        <div className={`h-screen ${backgroundColour}`}></div>
+        <div className={`h-screen ${backgroundColour} flex justify-center items-center`}>
+            <h1 className="p-4 bg-white rounded-2xl shadow-2xl text-4xl text-center">
+                Hope you enjoyed your early
+                <br></br>
+                Spotify Wrap!!!
+            </h1>
+        </div>
     )
 }
