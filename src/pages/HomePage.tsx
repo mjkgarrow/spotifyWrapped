7aea8a4354200d15f2b007e4e107d64d29c99b1f
socialtonics <mjkgarrow@gmail.com>
2023-09-17 20:18:44 +1000

    🚧: WIP mobile efficiency work

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,8 +2,8 @@ import { useGlobalState } from "../context/globalState"
 import CircleLoading from "../components/CircleLoading"
 import WelcomeSection from "../components/WelcomeSection"
 import EmptySection from "../components/EmptySection"
-import TopArtistsSection from "../components/TopArtistsSection"
-// import TopTracksSection from "../components/TopTracksSection"
+// import TopArtistsSection from "../components/TopArtistsSection"
+import TopTracksSection from "../components/TopTracksSection"
 // import ListeningSection from "../components/ListeningSection"
 
 
@@ -22,9 +22,9 @@ export default function HomePage() {
                     <div id='top'></div>
                     <WelcomeSection />
 
-                    {data.artists.length && <TopArtistsSection artists={data.artists.slice(0, 5)} />}
+                    {/* {data.artists.length && <TopArtistsSection artists={data.artists.slice(0, 5)} />} */}
 
-                    {/* {data.tracks.length && <TopTracksSection tracks={data.tracks.slice(0, 5)} />} */}
+                    {data.tracks.length && <TopTracksSection tracks={data.tracks.slice(0, 5)} />}
 
                     {/* {data.tracks.length && <ListeningSection tracks={data.tracks.slice(0, 99)} />} */}
 
