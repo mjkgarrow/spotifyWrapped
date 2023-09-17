709768c98780e79d68e39e644f1becf097b6b350
socialtonics <mjkgarrow@gmail.com>
2023-09-17 20:21:18 +1000

    🚧: WIP mobile efficiency work

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,8 +3,8 @@ import CircleLoading from "../components/CircleLoading"
 import WelcomeSection from "../components/WelcomeSection"
 import EmptySection from "../components/EmptySection"
 // import TopArtistsSection from "../components/TopArtistsSection"
-import TopTracksSection from "../components/TopTracksSection"
-// import ListeningSection from "../components/ListeningSection"
+// import TopTracksSection from "../components/TopTracksSection"
+import ListeningSection from "../components/ListeningSection"
 
 
 export default function HomePage() {
@@ -24,9 +24,9 @@ export default function HomePage() {
 
                     {/* {data.artists.length && <TopArtistsSection artists={data.artists.slice(0, 5)} />} */}
 
-                    {data.tracks.length && <TopTracksSection tracks={data.tracks.slice(0, 5)} />}
+                    {/* {data.tracks.length && <TopTracksSection tracks={data.tracks.slice(0, 5)} />} */}
 
-                    {/* {data.tracks.length && <ListeningSection tracks={data.tracks.slice(0, 99)} />} */}
+                    {data.tracks.length && <ListeningSection tracks={data.tracks.slice(0, 99)} />}
 
                     <EmptySection backgroundColour="bg-orange-400" />
                 </main>
