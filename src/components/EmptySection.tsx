45ddb607b7213306d3ce371088c9fc38216baa9d
socialtonics <mjkgarrow@gmail.com>
2023-09-17 17:11:31 +1000

    💄: tweaks text size on mobile

--- src/components/EmptySection.tsx
+++ src/components/EmptySection.tsx
@@ -8,7 +8,7 @@ export default function EmptySection(props: props) {
 
     return (
         <div className={`h-screen ${backgroundColour} flex justify-center items-center`}>
-            <h1 className="p-4 bg-white rounded-2xl shadow-2xl text-4xl text-center">
+            <h1 className="p-4 bg-white rounded-2xl shadow-2xl text-2xl text-center">
                 Hope you enjoyed your early
                 <br></br>
                 Spotify Wrap!!!
