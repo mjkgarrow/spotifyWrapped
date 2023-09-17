48ab1d66c323080283d8359fe74fd78add395944
socialtonics <mjkgarrow@gmail.com>
2023-09-17 18:50:30 +1000

    🐛: fixed typscript import error

--- src/components/TopTracksSection.tsx
+++ src/components/TopTracksSection.tsx
@@ -1,7 +1,7 @@
 import { useLayoutEffect, useRef } from "react";
 import { gsap } from "gsap"
 import { Track } from "../types/spotifyTypes";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { colours } from "../utils/globals";
 
 type props = {
