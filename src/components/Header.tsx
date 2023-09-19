98245654bb5d1010339164dcc247e304a0620bee
socialtonics <mjkgarrow@gmail.com>
2023-09-19 22:33:02 +1000

    💄: changed Spotify asset styles to conform to branding rules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
     return (
         <header className='w-screen flex h-16 justify-between items-center px-8 bg-[#1DB954] fixed z-50 top-0'>
             <div className='w-1/3'>
-                <img src='../../public/assets/images/logos/Spotify_Logo_RGB_Black.png' alt='spotify' className='w-28'></img>
+                <img src='/assets/images/logos/Spotify_Logo_RGB_Black.png' alt='spotify' className='w-28'></img>
             </div>
             <div className='w-1/3 flex justify-center'>
                 {isAuth() ? <TimeSelector /> : <div className='text-2xl sm:text-3xl font-bold min-w-max '>Wrap Me!</div>}
