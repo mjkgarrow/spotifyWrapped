290fe98029d4c7409cc40efba6ed574173745dde
socialtonics <mjkgarrow@gmail.com>
2023-09-19 22:22:30 +1000

    💄: updated Spotify branding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import LogoutBtn from '../components/LogoutBtn'
-import UserInfo from './UserInfo';
 import { useGlobalState } from '../context/globalState';
 import TimeSelector from './TimeSelector';
 
@@ -7,9 +6,9 @@ export default function Header() {
     const { isAuth } = useGlobalState()
 
     return (
-        <header className='w-screen flex h-16 justify-between items-center px-8 bg-[#1ed760] fixed z-50 top-0'>
+        <header className='w-screen flex h-16 justify-between items-center px-8 bg-[#1DB954] fixed z-50 top-0'>
             <div className='w-1/3'>
-                {isAuth() && <UserInfo />}
+                <img src='../../public/assets/images/logos/Spotify_Logo_RGB_Black.png' alt='spotify' className='w-28'></img>
             </div>
             <div className='w-1/3 flex justify-center'>
                 {isAuth() ? <TimeSelector /> : <div className='text-2xl sm:text-3xl font-bold min-w-max '>Wrap Me!</div>}
